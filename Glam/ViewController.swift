b343aef18d372af3486f4949723654b876769171
Student <student@rental-mac.local>
2020-10-29 11:42:35 -0600

    Initial Commit

---
+++ Glam/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Glam
+//
+//  Created by Student on 10/29/20.
+//  Copyright © 2020 Tucker Weibell. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
